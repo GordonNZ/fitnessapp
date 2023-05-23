c05f624bbd84d2aec486e83eee1fe744a17e20be
Gordon Zam <gordonzam@gmail.com>
2023-05-23 22:36:35 +1200

    functioning site, needs footer

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import HorizontalScrollBar from './HorizontalScrollBar';
+import Loader from './Loader';
 
-function SimilarExercises() {
-  return <div>SimilarExercises</div>;
+function SimilarExercises({ targetMuscleExercises, equipmentExercises }) {
+  return (
+    <Box sx={{ mt: { lg: '100px', xs: '50px' } }}>
+      <Typography variant='h3' mb={5}>
+        Exercises that target the same muscle group
+      </Typography>
+      <Stack direction='row' sx={{ p: '2', position: 'relative' }}>
+        {targetMuscleExercises.length ? (
+          <HorizontalScrollBar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+      <Typography variant='h3' mb={5}>
+        Exercises that use the same equipment
+      </Typography>
+      <Stack direction='row' sx={{ p: '2', position: 'relative' }}>
+        {targetMuscleExercises.length ? (
+          <HorizontalScrollBar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+    </Box>
+  );
 }
 
 export default SimilarExercises;
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 function ExerciseDetail() {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -22,14 +25,36 @@ function ExerciseDetail() {
         exerciseOptions
       );
       setExerciseDetail(exerciseDetailData);
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData.contents);
+
+      const targetMuscleExerciseData = await fetchData(
+        `${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,
+        exerciseOptions
+      );
+      setTargetMuscleExercises(targetMuscleExerciseData);
+      const equipmentExerciseData = await fetchData(
+        `${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`,
+        exerciseOptions
+      );
+      setEquipmentExercises(equipmentExerciseData);
     };
     fetchExercisesData();
   }, [id]);
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
-      <SimilarExercises />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      />
+      <SimilarExercises
+        targetMuscleExercises={targetMuscleExercises}
+        equipmentExercises={equipmentExercises}
+      />
     </Box>
   );
 }
